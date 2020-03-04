6a0201ca6392864474f5fa6b178ecbbff5fcef61
ArunTeltia <teltia.arun18@gmail.com>
2020-03-04 21:42:46 +0530

    DataBase Connection

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const connectDB = require('./config/db');
 const app     = express();
 
-const PORT    = process.env.PORT || 5000;
 
+connectDB();
 
 app.get('/',(req,res)=>res.send('API Running'));
-
+const PORT    = process.env.PORT || 5000;
 app.listen(PORT,()=> console.log(`Server started on port ${PORT}`));
